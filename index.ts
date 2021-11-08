a48d620e3372c46a84561ab0ca7f2849675cf908
ubermanu <manu@paca.happy-dev.fr>
2021-11-08 19:21:29 +0100

    move api into index

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
-export { Validator } from './src/Validator'
-export { validate } from './src/api'
+import { Schema } from './src/Schema'
+import { Validator } from './src/Validator'
+
+// Enable some backward compatibility with the previous API.
+const commonValidator = new Validator()
+
+export function validate(object: object, schema: Schema) {
+  return commonValidator.setSchema(schema).validate(object)
+}
+
+export default Validator
